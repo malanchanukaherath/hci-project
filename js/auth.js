c46f907efec9fa85f75b13f0ef2cb3a461ec5085
Chanuka Herath <malanherath4@gmail.com>
2026-04-05 16:45:52 +0530

    added landing page

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,6 @@
 (function () {
   var KEY = "fsc_portal_session";
+  var STAFF_KEY = "fsc_staff_session";
 
   window.PortalAuth = {
     isLoggedIn: function () {
@@ -20,8 +21,33 @@
       } catch (e) {}
     },
     requireAuth: function (loginPath) {
-      loginPath = loginPath || "index.html";
+      loginPath = loginPath || "student-login.html";
       if (!window.PortalAuth.isLoggedIn()) {
+        window.location.href = loginPath;
+        return false;
+      }
+      return true;
+    },
+    isStaffLoggedIn: function () {
+      try {
+        return sessionStorage.getItem(STAFF_KEY) === "1";
+      } catch (e) {
+        return false;
+      }
+    },
+    staffLogin: function () {
+      try {
+        sessionStorage.setItem(STAFF_KEY, "1");
+      } catch (e) {}
+    },
+    staffLogout: function () {
+      try {
+        sessionStorage.removeItem(STAFF_KEY);
+      } catch (e) {}
+    },
+    requireStaffAuth: function (loginPath) {
+      loginPath = loginPath || "staff-login.html";
+      if (!window.PortalAuth.isStaffLoggedIn()) {
         window.location.href = loginPath;
         return false;
       }
